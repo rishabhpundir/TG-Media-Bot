ecce089daad45dc803031bf2ccfe9837641f6659
rishabhpundir <th3rishabh@gmail.com>
2026-06-06 14:22:42 +0530

    fixed ffmpeg finding delay issue

--- ffmpeg/ffmpeg_find_delay.py
+++ ffmpeg/ffmpeg_find_delay.py
@@ -4,9 +4,7 @@ import subprocess
 import numpy as np
 from scipy.signal import correlate
 
-# Replaced hardcoded wildcard search with sys.argv for console arguments
 if len(sys.argv) != 3:
-    # Errors are sent to stderr so they don't corrupt the pure ms output on stdout
     sys.stderr.write("Usage: python find_delay.py <file1.mkv> <file2.mkv>\n")
     sys.exit(1)
 
@@ -16,9 +14,9 @@ file_2 = sys.argv[2]
 wav_1 = "temp_1.wav"
 wav_2 = "temp_2.wav"
 
-# Updated ffmpeg input variables and fixed the stream mapping
-subprocess.run(['ffmpeg', '-y', '-ss', '00:10:00', '-t', '00:05:00', '-i', file_1, '-map', '0:a:0', '-ar', '16000', '-ac', '1', wav_1], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-subprocess.run(['ffmpeg', '-y', '-ss', '00:10:00', '-t', '00:05:00', '-i', file_2, '-map', '0:a:0', '-ar', '16000', '-ac', '1', wav_2], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+# Added -vn, -sn, and -dn to force FFmpeg to completely ignore the heavy 4K HDR video streams
+subprocess.run(['ffmpeg', '-y', '-ss', '00:10:00', '-i', file_1, '-t', '00:05:00', '-map', '0:a:0', '-vn', '-sn', '-dn', '-ar', '16000', '-ac', '1', wav_1], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.run(['ffmpeg', '-y', '-ss', '00:10:00', '-i', file_2, '-t', '00:05:00', '-map', '0:a:0', '-vn', '-sn', '-dn', '-ar', '16000', '-ac', '1', wav_2], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 def load_wav(filename):
     with open(filename, 'rb') as f:
@@ -34,8 +32,6 @@ os.remove(wav_2)
 # Calculate cross-correlation to find where they match up
 correlation = correlate(audio_1, audio_2, mode='full')
 lag = np.argmax(correlation) - (len(audio_2) - 1)
-
-# Converted the delay to milliseconds and removed the muxing logic
 delay_ms = int(round((lag / 16000.0) * 1000))
 
 # Print strictly the millisecond value with the 'ms' suffix
